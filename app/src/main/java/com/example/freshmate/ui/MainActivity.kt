4f0539bc187a1282f96a3b7129390bb089cabb6c
LiloPratama <lilopratamaa@gmail.com>
2024-11-12 22:59:49 +0800

    Create Bottom Navigation

---
+++ app/src/main/java/com/example/freshmate/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.freshmate.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.freshmate.R
+import com.example.freshmate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home
+            )
+        )
+        navView.setupWithNavController(navController)
+    }
+}
